ff59be504caac5328bcb19dc18d1e0d569c4c94d
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2018-10-20 01:48:33 +0200

    adding first ball

--- Pinball_Jakes_Inferno/ModulePlayer.h
+++ Pinball_Jakes_Inferno/ModulePlayer.h
@@ -3,6 +3,8 @@
 #include "Globals.h"
 #include "p2Point.h"
 
+struct SDL_Texture;
+
 class ModulePlayer : public Module
 {
 public:
@@ -14,5 +16,13 @@ public:
 	bool CleanUp();
 
 public:
+	SDL_Texture *	ball_tex = nullptr;
+	uint			ball_sound;
+	int				ball_position_x;
+	int				ball_position_y;
 
+	PhysBody*		ball;
+	bool			ball_created = false;
+
+	
 };
